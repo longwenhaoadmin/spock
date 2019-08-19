8f9362b6267bd4191cfd3acd6f62897db6622dc0
longwh <longwh@esensoft.com>
2019-08-19 15:05:39 +0800

    提交classload

---
+++ src/main/java/classloader/StaticLoad.java
@@ -0,0 +1,16 @@
+package classloader;
+
+import classloader.bean.Singleton;
+import classloader.bean.Singleton2;
+
+public class StaticLoad {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println("Singleton1 value1:" + singleton.value1);
+        System.out.println("Singleton1 value2:" + singleton.value2);
+
+        Singleton2 singleton2 = Singleton2.getInstance2();
+        System.out.println("Singleton2 value1:" + singleton2.value1);
+        System.out.println("Singleton2 value2:" + singleton2.value2);
+    }
+}
